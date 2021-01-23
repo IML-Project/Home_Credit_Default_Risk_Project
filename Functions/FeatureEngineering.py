2210a5e59388b7aa21afaedf1c1eaec4749beadb
zalewski-lukasz <xlukzalx@gmail.com>
2021-01-23 20:06:51 +0100

    work in progress...

---
+++ Functions/FeatureEngineering.py
@@ -0,0 +1,69 @@
+import pandas as pd 
+import numpy as np
+
+# Generic functions, that will universally work for any data frame
+
+# Function used for grouping numerical values by passed statistics
+def group_numeric_values(data_frame,
+                         data_frame_name,
+                         groupby_id = 'SK_ID_CURR', 
+                         grouping_statistics = ['count', 'min', 'max', 'mean', 'median', 'sum']):
+    
+    isTarget = False
+    for column in data_frame:
+        if column == 'TARGET':
+            isTarget = True
+            target_cols = data_frame[[groupby_id, 'TARGET']]
+            data_frame = data_frame.drop(columns = 'TARGET')
+        if column != groupby_id and 'SK_ID' in column:
+            data_frame = data_frame.drop(columns = column)
+        
+    tmp_data_frame = data_frame.select_dtypes(exclude='object')
+    
+    grouped_data_frame = tmp_data_frame.groupby(groupby_id).agg(grouping_statistics)
+    
+    new_columns = []
+    
+    for variable_type in grouped_data_frame.columns.levels[0]:
+        if variable_type != groupby_id:
+            for stat_type in grouped_data_frame.columns.levels[1]:
+                col = f'{data_frame_name}_{variable_type}_{stat_type}'
+                new_columns.append(col)
+                
+    grouped_data_frame.columns = new_columns
+    if isTarget:
+        grouped_data_frame = grouped_data_frame.merge(target_cols, on = groupby_id, how = 'left')
+    return grouped_data_frame
+
+
+# Function used for grouping object values by passed statistics
+def group_object_values(data_frame,
+                        data_frame_name,
+                        groupby_id = 'SK_ID_CURR',
+                        grouping_statistics = ['count', 'mean', 'sum']):
+
+    grouped_data_frame = pd.get_dummies(data_frame.select_dtypes(include='object'))
+    
+    grouped_data_frame[groupby_id] = data_frame[groupby_id]
+    
+    grouped_data_frame = grouped_data_frame.groupby(groupby_id).agg(grouping_statistics)
+    
+    new_columns = []
+    
+    for variable_type in grouped_data_frame.columns.levels[0]:
+        if variable_type != groupby_id:
+            for stat_type in grouped_data_frame.columns.levels[1]:
+                col = f'{data_frame_name}_{variable_type}_{stat_type}'
+                new_columns.append(col)
+                
+    grouped_data_frame.columns = new_columns
+    return grouped_data_frame
+
+
+# Function used for finding correlations values between features in the given data frame and train set
+def data_correlation(train_frame, analysed_data_frame):
+    analysed_data_frame['TARGET'] = train_frame['TARGET']
+    analysed_data_frame = analysed_data_frame[analysed_data_frame['TARGET'].notna()]
+    correlations = analysed_data_frame.corr()['TARGET'].sort_values()
+    return correlations
+    
